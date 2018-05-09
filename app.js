391880084df25a3022f3312c398e830780f77d9d
Jacob <jakexmueller@gmail.com>
2018-05-09 11:01:17 -0500

    adding files

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+"use strict";
+
 // Code goes here
+
+let hourlyPay=prompt("Please enter your hourly wage information", "10")
+let hours=prompt("Please enter the number of hours you work in a week", "40")
+
+let weeklyWage= hourlyPay*hours;
+let weeklyWageRounded= weeklyWage.toFixed(2);
+
+
+
+console.log(weeklyWage);
+document.getElementById("test").innerHTML = weeklyWage;
+document.write("Your total pay is " + weeklyWageRounded);
+
 
 
